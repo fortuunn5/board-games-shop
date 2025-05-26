7b503359b0ab38c972bfa6db0416f0204e995ccc
Andreeva Viktoriia <viktoriia.andreeva@gmail.com>
2025-05-26 14:46:26 +0400

    Initial commit

---
+++ src/test/java/com/boardgames/boardgames/BoardGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgames.boardgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
